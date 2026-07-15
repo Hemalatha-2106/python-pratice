bbee9e6178a4413b18015d91af777fdee6e9e6a2
h <devinagalakshmi73@gmail.com>
2026-07-15 16:42:49 +0530

    Add files via upload

---
+++ conditionalstatement.py/if elif else.py.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the number"))
+if a>0:
+    print("Posstive number")
+elif a==0:
+    print("Zero")
+else:
+    print("Negative number")
---
+++ conditionalstatement.py/nested if.py
@@ -0,0 +1,8 @@
+age=int(input("Enter the number"))
+if age>=18:
+    if age>=60:
+        print("This person is senior")
+    else:
+        print("THis is person is not senior")
+else:
+    print("This person is minor")
